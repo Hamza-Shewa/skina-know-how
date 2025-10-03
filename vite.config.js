66ae642ec7e1fc9e32e0a888cf3181f7d76b94fb
hamza aboshhiwa <habushhiwa@gmail.com>
2025-10-03 19:07:36 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/skina-website/', // Replace 'skina-website' with your actual repository name
+  base: '/', // Replace 'skina-website' with your actual repository name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
